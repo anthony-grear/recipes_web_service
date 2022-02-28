040950c51f832af3430b995a8784b9fc90d85a53
Anthony Grear <tgrear2008@gmail.com>
2022-02-28 15:30:14 +0700

    Add date with annotation UpdateTimestamp. Add new field, category.

--- src/recipes/Recipe.java
+++ src/recipes/Recipe.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -30,6 +33,13 @@ public class Recipe {
 
     @NotBlank
     private String description;
+
+
+    @NotBlank
+    private String category;
+
+    @UpdateTimestamp
+    private LocalDateTime date;
 
     @ElementCollection
     @CollectionTable(name = "INGREDIENTS", joinColumns = @JoinColumn(name = "id"))
